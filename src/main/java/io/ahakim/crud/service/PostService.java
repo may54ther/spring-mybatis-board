708d538915735d112850b292515987ec21af4f4a
may54ther <aha9654@gmail.com>
2022-08-21 04:48:03 +0900

    Change: PostService
    (cherry picked from commit 672c3dccd95a37c653dbb60774e90d1a5ec4264a)

--- src/main/java/io/ahakim/crud/service/PostService.java
+++ src/main/java/io/ahakim/crud/service/PostService.java
@@ -1,9 +1,13 @@
 package io.ahakim.crud.service;
 
+import io.ahakim.crud.domain.Post;
+import io.ahakim.crud.form.PostUpdateForm;
 import io.ahakim.crud.mapper.PostMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -11,4 +15,34 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostMapper postMapper;
+
+
+    public int total() {
+        return postMapper.total();
+    }
+
+    public List<Post> findAll() {
+        return postMapper.selectAll(0, 10);
+    }
+
+    public Post findById(Long id) {
+        return postMapper.selectById(id);
+    }
+
+    public Post save(Post post) {
+        postMapper.insert(post);
+        return post;
+    }
+
+    public void update(PostUpdateForm form) {
+        postMapper.update(form);
+    }
+
+    public void updateViews(Long id) {
+        postMapper.updateViews(id);
+    }
+
+    public void remove(Long id) {
+        postMapper.delete(id);
+    }
 }
