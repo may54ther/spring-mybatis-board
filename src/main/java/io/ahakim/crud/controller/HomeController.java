53a7c917b069a14fe6e9efc1b1e250577648ebd3
may54ther <aha9654@gmail.com>
2022-08-21 04:17:50 +0900

    Add: requestMapping
    (cherry picked from commit 94ea5102ad7b9517baeda55a460456dbdf63e5c3)

--- src/main/java/io/ahakim/crud/controller/HomeController.java
+++ src/main/java/io/ahakim/crud/controller/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
 
     @RequestMapping
     public String home(Model model) {
-        return "redirect:/post";
+        return "redirect:/posts";
     }
 }
--- src/main/java/io/ahakim/crud/controller/PostController.java
+++ src/main/java/io/ahakim/crud/controller/PostController.java
@@ -1,10 +1,13 @@
 package io.ahakim.crud.controller;
 
+import io.ahakim.crud.form.PostSaveForm;
+import io.ahakim.crud.form.PostUpdateForm;
 import io.ahakim.crud.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -13,4 +16,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PostController {
 
     private final PostService postService;
+
+    @GetMapping
+    public String posts(Model model) {
+        return "views/post/list";
+    }
+
+    //조회
+    @GetMapping("/{id}")
+    public String post(@PathVariable long id, Model model) {
+        return "views/post/detail";
+    }
+
+    //등록
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("post", new PostSaveForm());
+        return "views/post/add";
+    }
+
+    @PostMapping("/add")
+    public String addPost(@ModelAttribute PostSaveForm form) {
+        return "redirect:{id}";
+    }
+
+    //수정
+    @GetMapping("/{id}/edit")
+    public String editForm(@PathVariable long id, Model model) {
+        model.addAttribute("post", new PostUpdateForm());
+        return "views/post/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String editPost(@PathVariable long id) {
+        return "redirect:{id}";
+    }
+
+    //삭제
+    @GetMapping("/{id}/remove")
+    public String remove(@PathVariable long id) {
+        return "redirect:/";
+    }
 }
