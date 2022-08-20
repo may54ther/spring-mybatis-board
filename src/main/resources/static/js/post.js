7ea5a43dfc97cd4557dbf5a970c1aeddd3989c23
may54ther <aha9654@gmail.com>
2022-08-21 04:14:59 +0900

    Feat: Toast UI Editor 적용
    (cherry picked from commit 92d89348c47098edc58da2b97400ec6346c0d9c8)

---
+++ src/main/resources/static/js/post.js
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////
+// Toast UI Editor & Viewer Initialize
+/////////////////////////////////////////////////
+const EL_VIEWER = document.querySelector('#viewer');
+const Viewer = EL_VIEWER ?
+    new toastui.Editor.factory({
+        el: document.querySelector('#viewer'),
+        viewer: true,
+        height: '500px',
+        initialEditType: "markdown",
+    }) : null;
+
+const EL_EDITOR = document.querySelector('#editor');
+const Editor = EL_EDITOR ?
+    new toastui.Editor({
+        el: EL_EDITOR,
+        previewStyle: 'vertical',
+        height: '500px',
+        initialEditType: "markdown",
+        initialValue: '',
+        events: {
+            change() {
+                //<![CDATA[
+                document.querySelector("#content").value = Editor.getHTML() || "";
+                //]]>
+            }
+        }
+    }) : null;
