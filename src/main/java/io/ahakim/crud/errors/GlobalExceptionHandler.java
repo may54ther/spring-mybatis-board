f4d4f46e64a9a67a7c8396f210c91e624c8a9b8b
may54ther <aha9654@gmail.com>
2023-01-07 18:53:09 +0900

    Feat: MVC, API 용으로 GlobalExceptionHandler 분리

--- src/main/java/io/ahakim/crud/errors/GlobalExceptionHandler.java
+++ src/main/java/io/ahakim/crud/errors/GlobalExceptionHandler.java
@@ -1,15 +1,26 @@
 package io.ahakim.crud.errors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@ControllerAdvice
+import java.util.Arrays;
+
+@Slf4j
+@ControllerAdvice("io.crud.ahakim.controller")
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler({Exception.class, RuntimeException.class})
+    public void handleException(Exception e) {
+        log.info("GlobalExceptionHandler.handleException");
+        log.info("e={} ", Arrays.toString(e.getStackTrace()));
+    }
+
+    // 예외
     @ExceptionHandler({PostNotFoundException.class})
-    public String handlePostNotFoundException(PostNotFoundException e, RedirectAttributes redirectAttrs) {
-        redirectAttrs.addFlashAttribute("msg", e.getMessage());
+    public String handlePostNotFoundException(PostNotFoundException e, RedirectAttributes redirect) {
+        redirect.addFlashAttribute("msg", e.getMessage());
         return "redirect:/posts";
     }
 }
