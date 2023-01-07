34df01380c4ee3cb2affd8dd714cd04db916b926
may54ther <aha9654@gmail.com>
2023-01-07 23:39:02 +0900

    Change: criteria 변수명 변경

--- src/main/java/io/ahakim/crud/domain/Criteria.java
+++ src/main/java/io/ahakim/crud/domain/Criteria.java
@@ -3,23 +3,25 @@ package io.ahakim.crud.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 public class Criteria {
 
     private int page;
-    private int rowSize;
+    private int size;
 
     public Criteria() {
         this(1, 10);
     }
 
-    public Criteria(int page, int rowSize) {
+    public Criteria(int page, int size) {
         this.page = page > 0 ? page : 1;
-        this.rowSize = Math.max(rowSize, 10);
+        this.size = Math.max(size, 10);
     }
 
     public int getStartRow() {
-        return (this.page - 1) * this.rowSize;
+        return (this.page - 1) * this.size;
     }
+
 }
--- src/main/java/io/ahakim/crud/domain/PageMaker.java
+++ src/main/java/io/ahakim/crud/domain/PageMaker.java
@@ -3,34 +3,39 @@ package io.ahakim.crud.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
 @Setter
 @Getter
-@Slf4j
 @NoArgsConstructor
 public class PageMaker {
 
     private int total;
+
     private int totalPage;
 
     private int pageRange;
-    private int rangeStart, rangeEnd;
-    private boolean prev, next;
+
+    private int rangeStart;
+
+    private int rangeEnd;
+
+    private boolean prev;
+
+    private boolean next;
 
     private Criteria criteria;
 
     public PageMaker(int total, Criteria criteria) {
         this.total = total;
         this.criteria = criteria;
-        
-        calculate(criteria.getPage(), criteria.getRowSize());
+
+        calculate();
     }
 
-    private void calculate(int page, int rowSize) {
-        this.totalPage = (int) Math.ceil((double) total / criteria.getRowSize());
+    private void calculate() {
+        this.totalPage = (int) Math.ceil((double) total / criteria.getSize());
         this.pageRange = Math.max(5, pageRange);
-        this.rangeStart = Math.floorDiv((page - 1), pageRange) * pageRange + 1;
+        this.rangeStart = Math.floorDiv((criteria.getPage() - 1), pageRange) * pageRange + 1;
         this.rangeEnd = Math.min((rangeStart + (pageRange - 1)), totalPage);
         this.prev = rangeStart > 1;
         this.next = rangeEnd != totalPage;
